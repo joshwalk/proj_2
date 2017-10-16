3be398316a4eb8cd0a5c254c38764b905b494e6e
Joshua Walker <joshwalk@umich.edu>
2017-10-15 20:16:17 -0400

    part 1 and 2 complete

--- p2.py
+++ p2.py
@@ -1,2 +1,14 @@
-#hey
-# here's something else
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'http://www.michigandaily.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "lxml")
+
+headline_list = []
+most_read_div = soup.find(class_="view-most-read")
+links = most_read_div.find_all('a')
+for link in links:
+    headline_list.append(link.text.strip())
+
+print(headline_list)
